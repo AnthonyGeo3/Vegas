6f405d75c6d8968b5da4593f29ba635a4edd6baa
Anthony <anthonygeorgiou@live.co.uk>
2023-12-06 20:06:06 +0000

    hours mins and secs added

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ function updateCountdown() {
     const difference = tripDate - now;
 
     const daysLeft = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hoursLeft = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minsLeft = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const secsLeft = Math.floor((difference % (1000 * 60)) / 1000);
 
-    document.getElementById('countdown').innerText = daysLeft + " days until Las Vegas!";
+    document.getElementById('countdown').innerText = daysLeft + " days " + hoursLeft + " hours " + minsLeft + " minutes " + secsLeft + " seconds until Las Vegas!";
 }
 
 // Update the countdown every second
